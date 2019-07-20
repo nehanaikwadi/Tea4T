254dbe69db6e3ff66022d11d549500fa5561c011
nehanaikwadi <nehanaikwadi97@gmail.com>
2019-07-20 09:59:30 +0530

    Interview links updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { PlayComponent } from './play/play.component';
+import { InterviewsComponent } from './interviews/interviews.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"signup",component:SignupComponent},
+  {path:"play",component:PlayComponent},
+  {path:"interviews",component:InterviewsComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { PlayComponent } from './play/play.component';
+import { InterviewsComponent } from './interviews/interviews.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    HomeComponent,
+    SignupComponent,
+    PlayComponent,
+    InterviewsComponent
   ],
   imports: [
     BrowserModule,
